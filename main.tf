83375ef323bba335f1ea4043b40ad4603076b88e
judif <jufreiberger@microsoft.com>
2023-02-27 16:33:05 +0100

    Fix var assignment

--- main.tf
+++ main.tf
@@ -9,16 +9,16 @@ module "vnet_hub_001" {
   source                  = "./modules/vnet"
   rg_name                 = azurerm_resource_group.main.name
   location                = azurerm_resource_group.main.location
-  vnet_name               = local.vnet_hub_001   
-  address_space           = local.as_hub_001_vnet
+  vnet_name               = var.vnet_hub_001   
+  address_space           = var.as_hub_001_vnet
 }
 
 module "snet_hub_001_default" {
   source          = "./modules/subnet"
   rg_name         = azurerm_resource_group.main.name
   location        = azurerm_resource_group.main.location
-  name            = local.snet_default
+  name            = var.snet_default
   vnet_name       = module.vnet_hub_001.name
-  address_space   = local.as_hub_001_snet_default
+  address_space   = var.as_hub_001_snet_default
   nsg             = true
 }
